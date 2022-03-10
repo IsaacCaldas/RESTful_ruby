1afd70498e7b23731344dc984c04ac351ab0345f
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-10 10:49:28 -0300

    Rack appname

---
+++ lib/app_name.rb
@@ -0,0 +1,26 @@
+# class AppName
+#   def initialize(app)
+#     @app = app
+#   end
+
+#   def call(env)
+#     ['200', {'Content-Type' => 'text/html'}, ["Hello, Ruby API here!"]]
+#   end
+# end
+class AppName
+  def initialize(app, app_name)
+    @app = app
+    @app_name = app_name
+  end
+
+  def call(env)
+    puts ">>>>> #{env}"
+    if env["ORIGINAL_FULLPATH"] == "/"
+      ['200', {'Content-Type' => 'text/html'}, ["Teste!"]]
+    else
+      status, headers, response = @app.call(env)
+      headers.merge!({'X-App-Name' => "#{@app_name}"})
+      [status, headers, [response.body]]
+    end
+  end
+end
